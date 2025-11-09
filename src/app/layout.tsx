3bc64c529ba558e6e6571612eba771e881b55ee1
Melissa Usher <MelEUsher17@gmail.com>
2025-11-09 08:29:44 -0700

    chore: tune Lighthouse CI to desktop preset and preconnect fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,15 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
+      </head>
       <body
         data-csp-nonce={nonce ?? undefined}
         className={`${inter.variable} ${kalam.variable} ${comfortaa.variable} bg-base-bg text-base-text font-sans antialiased`}
